3b15945f722928dc06fe63cfda3c5bf658dcec7a
Louze Akram <akramlouze53@gmail.com>
2025-08-17 19:47:44 +0000

    Error: Invalid src prop (https://github.com/akramFit/Akram-Fit-Training-

--- src/next.config.js
+++ src/next.config.js
@@ -26,6 +26,12 @@ const nextConfig = {
         hostname: 'raw.githubusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
