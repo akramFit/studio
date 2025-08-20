1b0f15c269abd390e096b270d230b51f35b9b4e7
Louze Akram <akramlouze53@gmail.com>
2025-08-20 15:01:42 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -3,11 +3,11 @@
 // Firebase project configuration.
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAMJz2BPiOxQana4fnFcJ1uyXqJMn0WfyM",
-  authDomain: "akramfit-ba687.firebaseapp.com",
-  projectId: "akramfit-ba687",
-  storageBucket: "akramfit-ba687.appspot.com",
-  messagingSenderId: "989866224138",
-  appId: "1:989866224138:web:833fedbeedc8bb18458df3",
-  measurementId: "G-Y8CCZN9LKL"
+  apiKey: "AIzaSyCueyAjxxqZvPUjkSmiZGuLxwawoUS8IaM",
+  authDomain: "akram-fitness.firebaseapp.com",
+  projectId: "akram-fitness",
+  storageBucket: "akram-fitness.appspot.com",
+  messagingSenderId: "268983106725",
+  appId: "1:268983106725:web:fada56ff930e2fad18214c",
+  measurementId: "G-2WH7P2SQYK"
 };
