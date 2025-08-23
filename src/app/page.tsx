6e2329c4d56c7f2971ee42e950ec2beae5dedd7b
Louze Akram <akramlouze53@gmail.com>
2025-08-23 15:42:23 +0000

    get back  the main landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,16 @@
-
-"use client";
-
-import dynamic from 'next/dynamic';
-import { Skeleton } from '@/components/ui/skeleton';
 import Navbar from '@/components/shared/Navbar';
 import HeroSection from '@/components/landing/HeroSection';
+import AboutSection from '@/components/landing/AboutSection';
+import AchievementsSection from '@/components/landing/AchievementsSection';
+import PricingSection from '@/components/landing/PricingSection';
+import GallerySection from '@/components/landing/GallerySection';
+import FaqSection from '@/components/landing/FaqSection';
+import SubscriptionForm from '@/components/landing/SubscriptionForm';
 import Footer from '@/components/shared/Footer';
-
-const AboutSection = dynamic(() => import('@/components/landing/AboutSection'), {
-  loading: () => <Skeleton className="h-[500px] w-full" />,
-});
-const AchievementsSection = dynamic(() => import('@/components/landing/AchievementsSection'), {
-  loading: () => <Skeleton className="h-[600px] w-full" />,
-});
-const PricingSection = dynamic(() => import('@/components/landing/PricingSection'), {
-  loading: () => <Skeleton className="h-[600px] w-full" />,
-});
-const GallerySection = dynamic(() => import('@/components/landing/GallerySection'), {
-  loading: () => <Skeleton className="h-[600px] w-full" />,
-});
-const FaqSection = dynamic(() => import('@/components/landing/FaqSection'), {
-  loading: () => <Skeleton className="h-[500px] w-full" />,
-  ssr: false,
-});
-const SubscriptionForm = dynamic(() => import('@/components/landing/SubscriptionForm'), {
-  loading: () => <Skeleton className="h-[700px] w-full" />,
-  ssr: false,
-});
-
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-black">
       <Navbar />
       <main className="flex-1">
         <HeroSection />
